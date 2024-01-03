586e1f5ab3531f689271f3502772ebd608910002
Douglas Camata <159076+douglascamata@users.noreply.github.com>
2024-01-03 20:27:09 +0100

    Add more unit tests

--- internal/checks/rule_label_value_type_test.go
+++ internal/checks/rule_label_value_type_test.go
@@ -24,9 +24,50 @@ func TestRuleLabelValueTypeCheck(t *testing.T) {
 						Last:  4,
 					},
 					Reporter: checks.RuleLabelValueTypeName,
-					Text:     "`foo` label value must be a string, got `!!bool`.",
+					Text:     "recording rule `foo` has label `foo` with non-string value, got `!!bool`.",
 					Severity: checks.Bug,
 				}}
+			},
+		},
+		{
+			description: "label is not a string in alerting rule / required",
+			content:     "- alert: foo\n  expr: rate(foo[1m]) > 0\n  labels:\n    foo: true\n",
+			checker: func(_ *promapi.FailoverGroup) checks.RuleChecker {
+				return checks.NewRuleLabelValueTypeCheck()
+			},
+			prometheus: noProm,
+			problems: func(uri string) []checks.Problem {
+				return []checks.Problem{{
+					Lines: parser.LineRange{
+						First: 4,
+						Last:  4,
+					},
+					Reporter: checks.RuleLabelValueTypeName,
+					Text:     "alerting rule `foo` has label `foo` with non-string value, got `!!bool`.",
+					Severity: checks.Bug,
+				}}
+			},
+		},
+		{
+			description: "label is a string in recording rule / required",
+			content:     "- record: foo\n  expr: rate(foo[1m]) > 0\n  labels:\n    foo: \"true\"\n",
+			checker: func(_ *promapi.FailoverGroup) checks.RuleChecker {
+				return checks.NewRuleLabelValueTypeCheck()
+			},
+			prometheus: noProm,
+			problems: func(uri string) []checks.Problem {
+				return nil
+			},
+		},
+		{
+			description: "label is a string in alerting rule / required",
+			content:     "- alert: foo\n  expr: rate(foo[1m]) > 0\n  labels:\n    foo: \"true\"\n",
+			checker: func(_ *promapi.FailoverGroup) checks.RuleChecker {
+				return checks.NewRuleLabelValueTypeCheck()
+			},
+			prometheus: noProm,
+			problems: func(uri string) []checks.Problem {
+				return nil
 			},
 		},
 	}
