5b227066f7a4b32a22b2f69b62a79eb5232f192d
Lukasz Mierzwa <lukasz@cloudflare.com>
2025-02-28 16:49:30 +0000

    Better promql syntax errors

--- internal/checks/promql_syntax.go
+++ internal/checks/promql_syntax.go
@@ -2,6 +2,9 @@ package checks
 
 import (
 	"context"
+	"errors"
+
+	promParser "github.com/prometheus/prometheus/promql/parser"
 
 	"github.com/cloudflare/pint/internal/discovery"
 	"github.com/cloudflare/pint/internal/output"
@@ -43,20 +46,38 @@ func (c SyntaxCheck) Reporter() string {
 func (c SyntaxCheck) Check(_ context.Context, _ discovery.Path, rule parser.Rule, _ []discovery.Entry) (problems []Problem) {
 	expr := rule.Expr()
 	if expr.SyntaxError != nil {
-		problems = append(problems, Problem{
-			Lines:    expr.Value.Lines,
-			Reporter: c.Reporter(),
-			Summary:  "PromQL syntax error",
-			Details:  SyntaxCheckDetails,
-			Diagnostics: []output.Diagnostic{
-				{
-					Message:     expr.SyntaxError.Error(),
+		diag := output.Diagnostic{
+			Message:     expr.SyntaxError.Error(),
+			Line:        expr.Value.Lines.First,
+			FirstColumn: expr.Value.Column,
+			LastColumn:  nodeLastColumn(expr.Value),
+		}
+
+		var perrs promParser.ParseErrors
+		ok := errors.As(expr.SyntaxError, &perrs)
+		if ok {
+			for _, perr := range perrs { // Use only the last error.
+				start := expr.Value.Column + int(perr.PositionRange.Start)
+				end := expr.Value.Column + int(perr.PositionRange.End)
+				if end > start {
+					end--
+				}
+				diag = output.Diagnostic{
+					Message:     perr.Err.Error(),
 					Line:        expr.Value.Lines.First,
-					FirstColumn: expr.Value.Column,
-					LastColumn:  nodeLastColumn(expr.Value),
-				},
-			},
-			Severity: Fatal,
+					FirstColumn: min(start, nodeLastColumn(expr.Value)),
+					LastColumn:  min(end, nodeLastColumn(expr.Value)),
+				}
+			}
+		}
+
+		problems = append(problems, Problem{
+			Lines:       expr.Value.Lines,
+			Reporter:    c.Reporter(),
+			Summary:     "PromQL syntax error",
+			Details:     SyntaxCheckDetails,
+			Diagnostics: []output.Diagnostic{diag},
+			Severity:    Fatal,
 		})
 	}
 	return problems
